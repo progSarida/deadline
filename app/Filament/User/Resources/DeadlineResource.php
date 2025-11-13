b3a2c7a8f8664a7720ecfb44c4399ff8559dfd75
progSarida <programmazione@sarida.it>
2025-11-13 14:05:26 +0100

    Correzione: tolta action edit da tabella risorsa

--- app/Filament/User/Resources/DeadlineResource.php
+++ app/Filament/User/Resources/DeadlineResource.php
@@ -303,7 +303,8 @@ class DeadlineResource extends Resource
             ])
             ->actions([
                 Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
