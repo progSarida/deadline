04446c8cac3c80335ef2d1842aab51a9ee8ee87e
mirkop <mirko85.dev@gmail.com>
2025-11-17 08:25:46 +0100

    upload per login centralizzato

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\SsoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/auth/callback', [SsoController::class, 'callback'])->name('sso.callback');
+Route::get('/sso-login', [SsoController::class, 'redirect'])->name('sso.login');
+
+Route::get('/admin/login', fn() => redirect()->route('sso.login'))->name('filament.admin.auth.login');
+
+Route::get('/login', fn() => redirect()->route('sso.login'));
